33f01fc0765c4c0b3df416fc8a8e3b6301b051aa
Тимур Белов <timurbl574@gmail.com>
2021-05-28 20:41:13 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
+
+const Setting = {
+  ERRORS_COUNT: 3,
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App
+      errorsCount={Setting.ERRORS_COUNT}
+    />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
